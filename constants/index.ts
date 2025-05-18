0a7687f5f5dcb975b53cb4dc3815e26b4ef2c5b1
Esau Jesse <jesseesau13@gmail.com>
2025-05-18 22:02:58 +0100

    Add Placeholder constants

---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+export const PLACEHOLDER = {
+    TEXT_SHORT: "Lorem ipsum",
+    TEXT_LONG: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    IMAGE_URL: "https://via.placeholder.com/150",
+    AVATAR_URL: "https://i.pravatar.cc/150?img=3",
+    DATE: new Date().toISOString(),
+    NUMBER: 42,
+    BOOLEAN: true,
+} as const;
